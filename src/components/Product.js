a6acaa0c90f44f3a28955d1a564fc6f9ab7b7389
Kaveenrajm99 <kaveenrajm99@gmail.com>
2022-06-13 22:29:17 +0530

    new

--- src/components/Product.js
+++ src/components/Product.js
@@ -20,7 +20,7 @@ const Product = () => {
     const ShowProduct = () => {
         return (
             <>
-                <div className='col-md-5'>
+                <div className='col-md-6'>
                     <img src={product.image} alt={product.title} height='400px' width='400px' />
                 </div>
                 <div className='col-md-6'>
